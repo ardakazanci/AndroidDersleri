d4ebbef46fe0d4c78d539c3911720c807e22458d
Arda Kazancı <ardakazanci@Arda-MacBook-Pro.local>
2017-02-04 21:08:05 +0300

    Android Dersleri - Android Tutorial - Explicit Intent - Arda Kazancı
    Android Dersleri - Android Tutorial - Explicit Intent - Arda Kazancı

--- ExplicitIntent/app/src/main/java/com/example/ardakazanci/explicitintent/MainActivity.java
+++ ExplicitIntent/app/src/main/java/com/example/ardakazanci/explicitintent/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.ardakazanci.explicitintent;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import static android.view.View.Y;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +37,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+
+
+//                Practice 1
+//
+//                Intent intent = new Intent(); // Yeni bir intent nesnesi
+//                ComponentName componentName = new ComponentName(MainActivity.this,OtherActivity.class);
+//                intent.setComponent(componentName);
+//                startActivity(intent);
+
+
+//                Practice 2
+
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this,OtherActivity.class);
+//                startActivity(intent);
+//
+//                Practice 3
+
+//                Intent intent = new Intent();
+//                intent.setClassName("com.example.ardakazanci.explicitintent","com.example.ardakazanci.OtherActivity"); // İlkinde package name ikincisinde class name
+//                startActivity(intent);
 
                 Intent intent = new Intent(MainActivity.this, OtherActivity.class);
                 startActivity(intent);
